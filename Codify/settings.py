5fbd88015f3c29577fee0aadafe953d78cccb3a4
Wes Griffin <43881864+Wesius@users.noreply.github.com>
2024-01-25 10:37:13 -0500

    changed settings.py

--- Codify/settings.py
+++ Codify/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-827tn5x#_v^y_oj#1znft+i30re2=&i7r(70p_6++gw7zhm!^7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["74.50.98.83"]
+ALLOWED_HOSTS = ["74.50.98.83", "localhost", "0.0.0.0", "127.0.0.1"]
 
 
 # Application definition
